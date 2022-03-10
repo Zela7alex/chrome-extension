6bbaabdae58e77ca7a942369cb22c43723573508
Alexandra Nunez <93490778+Zela7alex@users.noreply.github.com>
2022-03-10 17:48:06 -0500

    deployed chrome extension

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 
+
 //*** FUNCTION - save input button 
 inputBtn.addEventListener('click', function () {
     myLeads.push(inputEl.value);
-    console.log(myLeads)
+    // input value is reset
+    inputEl.value = " "
+    //When save button clicked, renderLeads() executes
+    renderLeads();
 })
 
-// Iterating through my leads and grabbing text from ulEl
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+//*** FUNCTION - render leads on page 
+function renderLeads() {
+    // list items variable to add in each lead
+    let listItems = ""
+    // Iterating through my leads and grabbing text from ulEl
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+            ${myLeads[i]}
+            </a>
+        </li>
+        `
+
+    }
+    ulEl.innerHTML = listItems
 }
-
-
