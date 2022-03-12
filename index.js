d41192255bbb15f757ce85220f1a4fe5c97c0bb2
Alexandra Nunez <93490778+Zela7alex@users.noreply.github.com>
2022-03-12 15:07:20 -0500

    cleared leads by create Delete All button

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 let myLeads = [];
+
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 
+// 2nd) Getting leads back from local storage
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const deleteBtn = document.getElementById('delete-btn')
 
-//*** FUNCTION - save input button 
+
+//*** 1st) FUNCTION - save input button pushes input values to array
 inputBtn.addEventListener('click', function () {
     myLeads.push(inputEl.value);
     // input value is reset
     inputEl.value = " "
-    //When save button clicked, renderLeads() executes
-    renderLeads();
+
+    // 1st) saving leads to local storage
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+
+    //When save button clicked, render() executes
+    render(myLeads);
+
 })
 
-
-//*** FUNCTION - render leads on page 
-function renderLeads() {
+//*** 2nd) FUNCTION - render leads/ any array you want. (We are passing in myLeads array through first argument "leads")
+function render(leads) { //leads takes in myLeads argument
     // list items variable to add in each lead
     let listItems = ""
     // Iterating through my leads and grabbing text from ulEl
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
         <li>
-            <a target='_blank' href='${myLeads[i]}'>
-            ${myLeads[i]}
+            <a target='_blank' href='${leads[i]}'>
+            ${leads[i]}
             </a>
         </li>
         `
@@ -31,3 +40,21 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems
 }
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads);
+
+}
+
+
+//*** 3rd) FUNCTION - clear localStorage, myLeads & DOM 
+deleteBtn.addEventListener('dblclick', function () {
+    localStorage.clear() // clears local storage
+
+    myLeads = [] // resets the myLeads array to 0
+
+    render(myLeads); // We can now clear the DOM by simply calling render() since in this function myLeads Array is set to 0 again.
+
+})
+
